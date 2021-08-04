57d802fafa64622c7e6a287bfc0d3c342e9513bf
stephenjjbrown <stephenjjbrown@me.com>
2021-08-04 11:21:46 -0700

    Creating initial documentation

--- src/trackable.ts
+++ src/trackable.ts
@@ -4,7 +4,7 @@ import { TrackedArray } from "./tracked-array";
 import { TrackedEntity } from "./tracked-entity";
 
 // Technically I think this would include TrackedArray now since TrackedArray no longer inherits from TrackedSubject
-// TODO: Use interface instead of Union Type. TrackedArray basically matches the interface of TrackedSubject anyway
+// TODO: Use interface instead of Union Type? TrackedArray basically matches the interface of TrackedSubject anyway
 export type Trackable<T> = TrackedSubject<T> | TrackedComputedSubject<T>;
 
 export type TrackedTypeForValue<T> = T extends (infer K)[] ? TrackedArray<K> : (TrackedSubject<T> | TrackedEntity<T>)
